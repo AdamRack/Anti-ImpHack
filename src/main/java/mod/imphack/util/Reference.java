1f95da4d8e36898bdaeba1ebf4e50492774f361a
Supergamer5465 <64598162+Supergamer5465@users.noreply.github.com>
2021-11-07 07:09:51 -0600

    update reference

--- src/main/java/mod/imphack/util/Reference.java
+++ src/main/java/mod/imphack/util/Reference.java
@@ -6,7 +6,7 @@ public class Reference {
 	public static final String MODID = "imphack";
 	public static final String NAME = "Anti-ImpHack";
 	public static final String DEVVERSION = "1.0.0";
-	public static final String VERSION = "1.0.0";
+	public static final String RELEASE_VERSION = "1.0.0";
 	public static final String ACCEPTVERSIONS = "[1.12.2]";
 	public static final ColorUtil IMPHACK_COLOR = new ColorUtil(121, 193, 255, 100);
 
