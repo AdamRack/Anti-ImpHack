813fe3f84fdc5438a73bef8111fb61f6b49f24ab
Supergamer5465 <64598162+Supergamer5465@users.noreply.github.com>
2021-11-14 12:26:36 -0600

    fix NPE

--- src/main/java/mod/imphack/util/font/FontUtils.java
+++ src/main/java/mod/imphack/util/font/FontUtils.java
@@ -4,20 +4,16 @@ import mod.imphack.Main;
 import mod.imphack.util.render.ColorUtil;
 import net.minecraft.client.Minecraft;
 
-
-
 public class FontUtils {
-	
-    public int fontSize = 16;
 
+	public int fontSize = 16;
 
 	private static final Minecraft mc = Minecraft.getMinecraft();
-	
+
 	public static float drawStringWithShadow(boolean customFont, String text, int x, int y, ColorUtil color) {
-		if(customFont) {
+		if (customFont) {
 			return Main.customFontRenderer.drawStringWithShadow(text, x, y, color);
-		}
-		else {
+		} else {
 			return mc.fontRenderer.drawStringWithShadow(text, x, y, color.getRGB());
 		}
 	}
@@ -25,8 +21,7 @@ public class FontUtils {
 	public static int getStringWidth(boolean customFont, String string) {
 		if (customFont) {
 			return Main.customFontRenderer.getStringWidth(string);
-		}
-		else {
+		} else {
 			return mc.fontRenderer.getStringWidth(string);
 		}
 	}
@@ -34,24 +29,21 @@ public class FontUtils {
 	public static int getFontHeight(boolean customFont) {
 		if (customFont) {
 			return Main.customFontRenderer.getHeight();
-		}
-		else {
+		} else {
 			return mc.fontRenderer.FONT_HEIGHT;
 		}
 	}
 
 	public static void drawStringWithShadow(String text, double x, double y, int color) {
-		
+
 	}
-	
+
 	public static float drawString(boolean customFont, String text, int x, int y, ColorUtil color) {
-		if(customFont) {
+		if (customFont && Main.customFontRenderer != null) {
 			return Main.customFontRenderer.drawString(text, x, y, color);
-		}
-		else {
+		} else {
 			return mc.fontRenderer.drawString(text, x, y, color.getRGB());
 		}
 	}
 
-	  
 }
